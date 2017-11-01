1730d96cbfe449d7db17c4b27c8668a9ec56c13a
Tech-A10 <tech-a10@Tech-A10s-MacBook-Air.local>
2017-11-01 22:35:37 +0000

    got the numbers to show up in the screen

--- dom-calculator-starter-code/js/calc.js
+++ dom-calculator-starter-code/js/calc.js
@@ -1,33 +1,61 @@
 document.addEventListener("DOMContentLoaded", function() {
   console.log('the content is loaded');
 
+var number = "";
+var numberOne = 0;
+var numberTwo = 0;
+var add = false;
+var minus = false;
+var divide = false;
+var times = false;
+
+// Buttons responding
 makeButtonsRespond();
 function makeButtonsRespond(){
+// Number buttons
   var buttonNum = document.querySelectorAll('.buttonNum')
   for (var i = 0; i < buttonNum.length; i++) {
     var button = buttonNum[i];
 
     button.addEventListener('click', function(event) {
-      console.log(this.value + ' was clicked');
+      number = number + this.value;
+      document.getElementById('screen').innerHTML = number;
+      console.log(number);
     })
   }
+// operations buttons
   var operator = document.querySelectorAll('.operator')
   for (var i = 0; i < operator.length; i++) {
     var operator_button = operator[i];
 
     operator_button.addEventListener('click', function(event) {
+      var numberOne = parseFloat(number);
+      number = "";
+      if (this.value === "+") {
+        var add = true;
+        var minus = false;
+        var divide = false;
+        var times = false;
+      }
+      var numberTwo = parseFloat(number);
       console.log(this.value + ' was clicked');
     })
   }
-  var equals = document.getElementsByClassName("equals");
-  equals[0].addEventListener("click", handleClick);
-  function handleClick() {
-    console.log("button was clicked")
-  }
+// equals button
+  var buttonEquals = document.getElementsByClassName("equals");
+  var button_equals = buttonEquals[0];
+  button_equals.addEventListener("click", function(event) {
+    var numberTwo = parseFloat(number);
+    if (add) {
+      document.getElementById('screen').innerHTML = "" + (numberOne + numberTwo);
+    }
+    console.log(this.value + ' was clicked')
+  })
+//clear button
   var buttonClear = document.getElementsByClassName("buttonClear");
   buttonClear[0].addEventListener("click", handleClick);
   function handleClick() {
-    console.log("clear button was clicked")
+    console.log(this.value + ' was clicked')
   }
 }
 // Calculator Functions
